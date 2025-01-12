52d3cb1f21cd9b8732f5cffb555e1f2630e90b8f
Gabo <omar.hinojoza13@gmail.com>
2025-01-12 08:56:28 -0600

    Remove facturas.php file and update Subtitle and Title components to prevent text selection by adding 'select-none' class.

--- src/app/components/Subtitle/index.tsx
+++ src/app/components/Subtitle/index.tsx
@@ -4,6 +4,7 @@ const defaultSubTitleClasses = '';
 
 export default function Subtitle(props: React.ComponentProps<"h2">) {
     const {className, ...rest} = props;
-    const classes = twMerge(defaultSubTitleClasses, className)
+    const _classes = twMerge(defaultSubTitleClasses, className)
+    const classes = twMerge(_classes, 'select-none')
     return <h2 className={classes} {...rest}></h2>;
 }
--- src/app/components/Title/index.tsx
+++ src/app/components/Title/index.tsx
@@ -4,6 +4,7 @@ const defaultTitleClasses = 'drop-shadow';
 
 export default function Title(props: React.ComponentProps<"h1">) {
     const {className, ...rest} = props;
-    const classes = twMerge(defaultTitleClasses, className)
+    const _classes = twMerge(defaultTitleClasses, className)
+    const classes = twMerge(_classes, 'select-none')
     return <h1 className={classes} {...rest}></h1>;
 }
